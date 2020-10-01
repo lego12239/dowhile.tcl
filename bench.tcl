f48bab95cc7f73ea932ef3f843a01469c7d9e1c8
Oleg Nemanov <lego12239@yandex.ru>
2020-10-01 14:53:36 +0300

    add nested loops in the benchmark

--- bench.tcl
+++ bench.tcl
@@ -7,3 +7,9 @@ puts "while"
 puts [time {set i 5000; while {[incr i -1]} {}} 100]
 puts "do-while"
 puts [time {set i 5000; do {} while {[incr i -1]}} 100]
+
+puts "nested loop"
+puts "while"
+puts [time {set i 100; while {[incr i -1]} {set j 1000; while {[incr j -1]} {}}} 100]
+puts "do-while"
+puts [time {set i 100; do {set j 1000; do {} while {[incr j -1]}} while {[incr i -1]}} 100]
